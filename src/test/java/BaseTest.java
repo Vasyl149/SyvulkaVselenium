92f574d149a34714839f240886dc1372202d1ffa
vasl <syvulkav@gmail.com>
2022-02-20 22:15:08 +0200

    refactoring 5

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -12,12 +12,11 @@ import static config.ConfManager.conf;
 
 public abstract class BaseTest {
     protected static final Logger logger = Logger.getLogger(MainMenuBO.class);
-   // private final String env = "Chrome";
-
+    private final String env = "Chrome";
+   // private  final String env = System.getProperty("env");
     @BeforeMethod
     //  @Parameters("browser")
     public void preCondition() {
-        String env = System.getProperty("env");
         WebDriver driver = new BrowserInstance().createInstance(env);
         DriverManager.setDriver(driver);
 
