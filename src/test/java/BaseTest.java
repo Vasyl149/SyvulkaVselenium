2d7cd7afe95e83d16fa0ee77c824b9215cff84c7
vasl <syvulkav@gmail.com>
2022-02-22 20:45:35 +0200

    refactoring 8

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import page_bo.main_pages.commonBO.MainMenuBO;
 import page_bo.main_pages.loginPageBO.LoginPageBO;
 
@@ -11,11 +12,11 @@ import static config.ConfigManager.conf;
 
 public abstract class BaseTest {
     protected static final Logger logger = Logger.getLogger(MainMenuBO.class);
-    // private final String browser = "Chrome";
-    private final String browser = System.getProperty("environment");
+     private final String browser = "Edge";
+    //private final String browser = System.getProperty("environment");
 
     @BeforeMethod
-    //@Parameters("browser")
+    @Parameters("browser")
     public void preCondition() {
         WebDriver driver = new BrowserInstance().createInstance(browser);
         DriverManager.setDriver(driver);
